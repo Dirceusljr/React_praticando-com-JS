b5de639547f002ac6b1c8450f70ef2a0c705f9a7
Dirceusljr <dirjunior18@gmail.com>
2024-03-11 18:37:05 -0300

    aula 01

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,14 @@
+import Cabecalho from 'components/Cabecalho'
+import Rodape from 'components/Rodape'
+import React from 'react'
+
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  )
+}
+
+export default Inicio
