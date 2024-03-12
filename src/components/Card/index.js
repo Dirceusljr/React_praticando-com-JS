848313d4283e07c993d8e01f09eb1fab019a7ef3
Dirceusljr <dirjunior18@gmail.com>
2024-03-12 15:31:27 -0300

    contexto, hook personalisado e página Favoritos

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,24 @@
 import styles from './Card.module.css'
 import iconeFavoritar from './favoritar.png'
-
+import iconeDesfavoritar from './desfavoritar.png'
 import React from 'react'
+import { useFavoritoContext } from 'contextos/Favoritos'
 
 function Card({ id, capa, titulo}) {
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  const ehFavorito = favorito.some((fav) => fav.id === id );
+  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
   return (
     <div className={styles.container}>
         <img src={capa} alt={titulo} className={styles.capa} />
         <h2>{titulo}</h2>
-        <img src={iconeFavoritar} 
+        <img src={icone} 
             alt='Favoritar Filme'
-            className={styles.favoritar} />
+            className={styles.favoritar}
+            onClick={() => {
+              adicionarFavorito({id, capa, titulo})
+              }} />
     </div>
   )
 }
